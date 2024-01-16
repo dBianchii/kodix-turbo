6938ee17e3c67eebc4b58bd212b00a459b78ba5d
Gabriel Bianchi <gdbianchii@gmail.com>
2024-01-16 08:23:31 -0300

    and dont import them

--- apps/kdx/src/app/layout.tsx
+++ apps/kdx/src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata, Viewport } from "next";
-import { cache } from "react";
-import { headers } from "next/headers";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GeistMono } from "geist/font/mono";
