b10c2d4763d37bb171d0b53451786549206abfa4
Gabriel Bianchi <gdbianchii@gmail.com>
2024-01-16 08:36:44 -0300

    require await off

--- tooling/eslint/nextjs.js
+++ tooling/eslint/nextjs.js
@@ -3,6 +3,7 @@ const config = {
   extends: ["plugin:@next/next/core-web-vitals"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/require-await": "off",
   },
 };
 
