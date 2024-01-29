af2a652d4eb72ac327b5ee7fedc06e5bf03f83fc
dBianchii <gdbianchii@gmail.com>
2024-01-29 18:03:19 -0300

    fix

--- apps/kdx/next.config.js
+++ apps/kdx/next.config.js
@@ -4,7 +4,7 @@ import "@kdx/auth/env";
 
 //TODO: remove once prisma fixes this https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-monorepo
 //TODO: https://github.com/vercel/next.js/issues/46070
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import("next").NextConfig} */
 const config = {
