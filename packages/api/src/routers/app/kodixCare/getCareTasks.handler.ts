45dc958b92303dcbe7ab00cbb40ec35e3b35d7c6
dBianchii <gdbianchii@gmail.com>
2024-03-10 17:43:45 -0300

    yes

--- packages/api/src/routers/app/kodixCare/getCareTasks.handler.ts
+++ packages/api/src/routers/app/kodixCare/getCareTasks.handler.ts
@@ -1,6 +1,10 @@
 import type { TGetCareTasksInputSchema } from "@kdx/validators/trpc/app/kodixCare";
+import dayjs from "@kdx/dayjs";
+import { kodixCareAppId } from "@kdx/shared";
 
 import type { TProtectedProcedureContext } from "../../../trpc";
+import { getAllHandler } from "../calendar/getAll.handler";
+import { getConfigHandler } from "../getConfig.handler";
 
 interface GetCareTasksOptions {
   ctx: TProtectedProcedureContext;
@@ -11,24 +15,49 @@ export const getCareTasksHandler = async ({
   ctx,
   input,
 }: GetCareTasksOptions) => {
-  //TODO: understand yeah
-  const careTasks = await ctx.prisma.careTask.findMany({
-    where: {
-      EventMaster: {
-        teamId: ctx.session.user.activeTeamId,
+  const calendarTasks = await getAllHandler({ ctx, input });
+
+  const careTasks = (
+    await ctx.prisma.careTask.findMany({
+      where: {
+        EventMaster: {
+          teamId: ctx.session.user.activeTeamId,
+        },
+        eventDate: {
+          gte: input.dateStart,
+          lte: input.dateEnd,
+        },
       },
-      eventDate: {
-        gte: input.dateStart,
-        lte: input.dateEnd,
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        eventDate: true,
       },
-    },
-    select: {
-      id: true,
-      title: true,
-      description: true,
-      eventDate: true,
-    },
+    })
+  ).map((task) => ({
+    title: task.title,
+    description: task.description,
+    date: task.eventDate,
+  }));
+
+  const config = await getConfigHandler({
+    ctx,
+    input: { appId: kodixCareAppId },
   });
 
-  return careTasks;
+  const union = [
+    ...careTasks,
+    ...calendarTasks.filter(
+      (ct) =>
+        !config.clonedCareTasksUntil ||
+        dayjs(ct.date).isAfter(config.clonedCareTasksUntil),
+    ),
+  ].map((tasks) => ({
+    title: tasks.title,
+    description: tasks.description,
+    date: tasks.date,
+  }));
+
+  return union;
 };
