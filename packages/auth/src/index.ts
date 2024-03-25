6cf87e4a71fcc12f9d13126ca400496d982740ce
Gabriel Bianchi <gdbianchii@gmail.com>
2024-03-25 16:50:01 -0300

    fix

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,4 +1,3 @@
-import { cache } from "react";
 import NextAuth from "next-auth";
 
 import { authConfig } from "./config";
@@ -7,15 +6,9 @@ export type { Session } from "next-auth";
 
 const {
   handlers: { GET, POST },
-  auth: defaultAuth,
+  auth,
   signIn,
   signOut,
 } = NextAuth(authConfig);
-
-/**
- * This is the main way to get session data for your RSCs.
- * This will de-duplicate all calls to next-auth's default `auth()` function and only call it once per request across all components
- */
-const auth = cache(defaultAuth);
 
 export { GET, POST, auth, signIn, signOut };
