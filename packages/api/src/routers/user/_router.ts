5e5cc98d3b45a42c8d6f11dd9fdd802f88480e48
Gabriel Bianchi <gdbianchii@gmail.com>
2024-04-18 09:33:19 -0300

    remove some unused endpoints

--- packages/api/src/routers/user/_router.ts
+++ packages/api/src/routers/user/_router.ts
@@ -2,24 +2,19 @@ import type { TRPCRouterRecord } from "@trpc/server";
 
 import {
   ZChangeNameInputSchema,
-  ZGetOneInputSchema,
   ZSwitchActiveTeamInputSchema,
 } from "@kdx/validators/trpc/user";
 
-import { protectedProcedure, publicProcedure } from "../../procedures";
+import { protectedProcedure } from "../../procedures";
 import { changeNameHandler } from "./changeName.handler";
-import { getAllHandler } from "./getAll.handler";
 import { getNotificationsHandler } from "./getNotifications.handler";
-import { getOneHandler } from "./getOne.handler";
 import { switchActiveTeamHandler } from "./switchActiveTeam.handler";
 
 export const userRouter = {
   changeName: protectedProcedure
     .input(ZChangeNameInputSchema)
     .mutation(changeNameHandler),
-  getAll: publicProcedure.query(getAllHandler),
   getNotifications: protectedProcedure.query(getNotificationsHandler),
-  getOne: protectedProcedure.input(ZGetOneInputSchema).query(getOneHandler),
   switchActiveTeam: protectedProcedure
     .input(ZSwitchActiveTeamInputSchema)
     .mutation(switchActiveTeamHandler),
--- packages/validators/src/trpc/user/index.ts
+++ packages/validators/src/trpc/user/index.ts
@@ -2,10 +2,8 @@ import { z } from "zod";
 
 import { ZNanoId } from "../..";
 
-export const ZGetOneInputSchema = z.object({
-  userId: ZNanoId,
-});
-export type TGetOneInputSchema = z.infer<typeof ZGetOneInputSchema>;
+export const ZChangeNameInputSchema = z.object({ name: z.string().max(32) });
+export type TChangeNameInputSchema = z.infer<typeof ZChangeNameInputSchema>;
 
 export const ZSwitchActiveTeamInputSchema = z.object({
   teamId: ZNanoId,
@@ -13,11 +11,3 @@ export const ZSwitchActiveTeamInputSchema = z.object({
 export type TSwitchActiveTeamInputSchema = z.infer<
   typeof ZSwitchActiveTeamInputSchema
 >;
-
-export const ZInstallAppInputSchema = z.object({
-  appId: ZNanoId,
-});
-export type TInstallAppInputSchema = z.infer<typeof ZInstallAppInputSchema>;
-
-export const ZChangeNameInputSchema = z.object({ name: z.string().max(32) });
-export type TChangeNameInputSchema = z.infer<typeof ZChangeNameInputSchema>;
