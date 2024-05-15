1565e2ccdc9a341aa95832051dcd30adcaf8b2f3
Gabriel Bianchi <gdbianchii@gmail.com>
2024-05-15 12:31:08 -0300

    Some formatting

--- apps/kdx/src/app/[locale]/_components/posthog-provider.tsx
+++ apps/kdx/src/app/[locale]/_components/posthog-provider.tsx
@@ -5,9 +5,7 @@ import { PostHogProvider } from "posthog-js/react";
 
 import { env } from "~/env";
 
-if (
-  typeof window !== "undefined"
-) {
+if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
   });
