7eaa0bf24816b3e63ff4b4f51f0239e652eac621
Gabriel Bianchi <gdbianchii@gmail.com>
2024-05-31 15:38:18 -0300

    remove test thing

--- apps/kdx/src/app/[locale]/team/notifications/_actions/deleteNotificationsAction.ts
+++ apps/kdx/src/app/[locale]/team/notifications/_actions/deleteNotificationsAction.ts
@@ -9,8 +9,6 @@ import { api } from "~/trpc/server";
 export const deleteNotificationsAction = async (
   input: TDeleteNotificationsInputSchema,
 ) => {
-  //wait 4 secs
-  await new Promise((resolve) => setTimeout(resolve, 4000));
   await api.user.deleteNotifications(input);
   revalidatePath("/[locale]/team/notifications");
 };
