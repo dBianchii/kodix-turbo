8262808364c87de88f0866a79fb94601e75c1571
Gabriel Bianchi <gdbianchii@gmail.com>
2024-06-09 23:33:42 -0300

    fix: auth trust host only in dev

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -157,8 +157,12 @@ export const isSecureContext = env.NODE_ENV !== "development";
 export const authConfig = {
   adapter,
   // In development, we need to skip checks to allow Expo to work
-  skipCSRFCheck: isSecureContext ? undefined : skipCSRFCheck,
-  trustHost: !isSecureContext,
+  ...(!isSecureContext
+    ? {
+        skipCSRFCheck: skipCSRFCheck,
+        trustHost: true,
+      }
+    : {}),
   secret: env.AUTH_SECRET,
   providers: [
     Google({
