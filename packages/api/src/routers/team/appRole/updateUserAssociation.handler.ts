d1c41e6591e86075ecec7bcbdeb8e98bbc6bae73
dbianchii <dbianchii@gmail.com>
2024-07-18 20:21:31 -0300

    OOK

--- packages/api/src/routers/team/appRole/updateUserAssociation.handler.ts
+++ packages/api/src/routers/team/appRole/updateUserAssociation.handler.ts
@@ -1,6 +1,9 @@
+import { TRPCError } from "@trpc/server";
+
 import type { TUpdateUserAssociationInputSchema } from "@kdx/validators/trpc/team/appRole";
 import { eq } from "@kdx/db";
 import { schema } from "@kdx/db/schema";
+import { appIdToAdminRole_defaultIdMap } from "@kdx/shared";
 
 import type { TIsTeamOwnerProcedureContext } from "../../../procedures";
 
@@ -14,6 +17,30 @@ export const updateUserAssociationHandler = async ({
   input,
 }: UpdateUserAssociationOptions) => {
   await ctx.db.transaction(async (tx) => {
+    if (input.userId === ctx.session.user.id) {
+      //need to detect if they are sending the admin role to prevent removing themselves
+      const adminTeamAppRolesForApp = await tx
+        .select({ id: schema.teamAppRoles.id })
+        .from(schema.teamAppRoles)
+        .where(
+          eq(
+            schema.teamAppRoles.appRoleDefaultId,
+            appIdToAdminRole_defaultIdMap[input.appId],
+          ),
+        );
+
+      if (
+        !adminTeamAppRolesForApp.some((x) =>
+          input.teamAppRoleIds.includes(x.id),
+        )
+      ) {
+        throw new TRPCError({
+          message: "You cannot remove yourself from the Administrator role",
+          code: "BAD_REQUEST",
+        });
+      }
+    }
+
     await tx
       .delete(schema.teamAppRolesToUsers)
       .where(eq(schema.teamAppRolesToUsers.userId, input.userId));
