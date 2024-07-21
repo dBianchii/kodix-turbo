62a0b1c00abdb7e489e2d9d817b81a526ed569e9
Gabriel Bianchi <gdbianchii@gmail.com>
2024-07-21 11:41:15 -0300

    KOD-74 Usuário perde a permissão de forma equivocada (bug)

--- packages/api/src/routers/team/appRole/updateUserAssociation.handler.ts
+++ packages/api/src/routers/team/appRole/updateUserAssociation.handler.ts
@@ -1,5 +1,5 @@
 import type { TUpdateUserAssociationInputSchema } from "@kdx/validators/trpc/team/appRole";
-import { eq } from "@kdx/db";
+import { and, eq, inArray } from "@kdx/db";
 import { schema } from "@kdx/db/schema";
 
 import type { TIsTeamOwnerProcedureContext } from "../../../procedures";
@@ -14,9 +14,27 @@ export const updateUserAssociationHandler = async ({
   input,
 }: UpdateUserAssociationOptions) => {
   await ctx.db.transaction(async (tx) => {
+    const teamAppRolesForTeamAndAppQuery = tx
+      .select({ id: schema.teamAppRoles.id })
+      .from(schema.teamAppRoles)
+      .where(
+        and(
+          eq(schema.teamAppRoles.appId, input.appId),
+          eq(schema.teamAppRoles.teamId, ctx.session.user.activeTeamId),
+        ),
+      );
+
     await tx
       .delete(schema.teamAppRolesToUsers)
-      .where(eq(schema.teamAppRolesToUsers.userId, input.userId));
+      .where(
+        and(
+          eq(schema.teamAppRolesToUsers.userId, input.userId),
+          inArray(
+            schema.teamAppRolesToUsers.teamAppRoleId,
+            teamAppRolesForTeamAndAppQuery,
+          ),
+        ),
+      );
 
     if (input.teamAppRoleIds.length)
       // If there are any teamAppRoleIds to connect, insert them after deletion
