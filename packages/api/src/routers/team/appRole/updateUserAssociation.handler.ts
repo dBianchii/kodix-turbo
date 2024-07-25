b400332c9d8cac6ab57f5943e81d25fcf6fb5afa
Gabriel Bianchi <gdbianchii@gmail.com>
2024-07-25 20:28:48 -0300

    KOD-66 Em [Configurações da equipe] / [Papeis] / [Permissões] - Não pode permitir retirar o administrador. Administrador deve sempre ter direito a tudo. (#259)
    * kod-66-em-configuracoes-da-equipe-papeis-permissoes-nao-pode
    
    * kod-63-nas-configuracoes-da-equipe-quando-tenho-2-usuarios-para-um
    
    * OOK
    
    * RM
    
    * Fix
    
    ---------
    
    Co-authored-by: dbianchii <dbianchii@gmail.com>

--- packages/api/src/routers/team/appRole/updateUserAssociation.handler.ts
+++ packages/api/src/routers/team/appRole/updateUserAssociation.handler.ts
@@ -1,6 +1,9 @@
+import { TRPCError } from "@trpc/server";
+
 import type { TUpdateUserAssociationInputSchema } from "@kdx/validators/trpc/team/appRole";
 import { eq } from "@kdx/db";
-import { teamAppRolesToUsers } from "@kdx/db/schema";
+import { teamAppRoles, teamAppRolesToUsers } from "@kdx/db/schema";
+import { appIdToAdminRole_defaultIdMap } from "@kdx/shared";
 
 import type { TIsTeamOwnerProcedureContext } from "../../../procedures";
 
@@ -8,12 +11,35 @@ interface UpdateUserAssociationOptions {
   ctx: TIsTeamOwnerProcedureContext;
   input: TUpdateUserAssociationInputSchema;
 }
-
 export const updateUserAssociationHandler = async ({
   ctx,
   input,
 }: UpdateUserAssociationOptions) => {
   await ctx.db.transaction(async (tx) => {
+    if (input.userId === ctx.session.user.id) {
+      //need to detect if they are sending the admin role to prevent removing themselves
+      const adminTeamAppRolesForApp = await tx
+        .select({ id: teamAppRoles.id })
+        .from(teamAppRoles)
+        .where(
+          eq(
+            teamAppRoles.appRoleDefaultId,
+            appIdToAdminRole_defaultIdMap[input.appId],
+          ),
+        );
+
+      if (
+        !adminTeamAppRolesForApp.some((x) =>
+          input.teamAppRoleIds.includes(x.id),
+        )
+      ) {
+        throw new TRPCError({
+          message: "You cannot remove yourself from the Administrator role",
+          code: "BAD_REQUEST",
+        });
+      }
+    }
+
     await tx
       .delete(teamAppRolesToUsers)
       .where(eq(teamAppRolesToUsers.userId, input.userId));
