4ed30131076fb24db77ab0ec5edff5b2cefd9e28
Gabriel Bianchi <gdbianchii@gmail.com>
2024-07-30 11:36:02 -0300

    expo_useI18nZodErrors

--- packages/validators/src/utils/useI18nZodErrors.ts
+++ packages/validators/src/utils/useI18nZodErrors.ts
@@ -1,6 +1,7 @@
-import { useTranslations } from "next-intl";
 import { z } from "zod";
 
+import { useTranslations } from "@kdx/locales/client";
+import { useTranslations as expo_useTranslations } from "@kdx/locales/expo/use-intl";
 import { getTranslations } from "@kdx/locales/server";
 
 import { makeZodI18nMap } from "./make-zod-error-map";
@@ -16,5 +17,12 @@ export const createI18nZodErrors = async ({ locale }: { locale: string }) => {
   const t = await getTranslations({ locale, namespace: "zod" });
   const tForm = await getTranslations({ locale, namespace: "form" });
   const tCustom = await getTranslations({ locale, namespace: "customErrors" });
+  z.setErrorMap(makeZodI18nMap({ t, tForm, tCustom }));
+};
+
+export const expo_useI18nZodErrors = () => {
+  const t = expo_useTranslations("zod");
+  const tForm = expo_useTranslations("form");
+  const tCustom = expo_useTranslations("customErrors");
   z.setErrorMap(makeZodI18nMap({ t, tForm, tCustom }));
 };
