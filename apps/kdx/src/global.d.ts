7fa6168c6139a503af71e7422c9700a59621685d
dbianchii <dbianchii@gmail.com>
2024-07-31 16:45:40 -0300

    lint fixing

--- apps/kdx/src/global.d.ts
+++ apps/kdx/src/global.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 //! Next/navigation is acting icky, and using pages router types sometimes.
 //! usePathname and useSearchParams sometimes return null in pages router. I believe the types are wrong because of 'node-linker=hoisted' in .npmrc
 import type { ReadonlyURLSearchParams } from "next/navigation";
