dde6e6697c8912fc59c51579d7d69874a84a21b7
dbianchii <dbianchii@gmail.com>
2024-07-31 20:54:43 -0300

    aaa

--- apps/kdx/src/middleware.ts
+++ apps/kdx/src/middleware.ts
@@ -20,7 +20,8 @@ export const config = {
     // - … if they start with `/api`, `/_next` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
     "/((?!api|_next|_vercel|.*\\..*).*)",
+
     // Match all pathnames within `/users`, optionally with a locale prefix
-    "/(.+)?/users/(.+)",
+    // "/(.+)?/users/(.+)",
   ],
 };
--- packages/locales/src/index.ts
+++ packages/locales/src/index.ts
@@ -9,3 +9,16 @@ type TranslationKeys = never;
 export type IsomorficT<S extends TranslationKeys = never> =
   | Awaited<ReturnType<typeof getTranslations<S>>>
   | ReturnType<typeof useTranslations<S>>; //This type is the same type as use-intl's useTranslations.
+
+// type Paths<Schema, Path extends string = ""> = Schema extends string
+//   ? Path
+//   : Schema extends object
+//     ? {
+//         [K in keyof Schema & string]: Paths<
+//           Schema[K],
+//           `${Path}${Path extends "" ? "" : "."}${K}`
+//         >;
+//       }[keyof Schema & string]
+//     : never;
+
+// type Messages = Paths<IntlMessages>;
