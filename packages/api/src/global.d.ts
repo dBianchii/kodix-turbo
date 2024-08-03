76105032d4b731fde66b447ee16f82d0ed026a54
Gabriel Bianchi <gdbianchii@gmail.com>
2024-08-03 11:56:50 -0300

    remove api because... no need

--- packages/api/src/global.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-imports */
-// Use type safe message keys with `next-intl`
-
-//? @kdx/api should only use "api" namespace
-type ApiMessages = typeof import("../../locales/src/messages/api/en.json");
-
-type IntlMessages = ApiMessages;
--- packages/api/src/root.ts
+++
@@ -1,9 +0,0 @@
-import { appRouter as _appRouter } from "./routers/app/_router"; //sad that I had to alias it lol
-import { createTRPCRouter } from "./trpc";
-
-export const appRouter = createTRPCRouter({
-  app: _appRouter,
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
