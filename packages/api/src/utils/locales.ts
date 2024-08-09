6b4cccebb4b81aa51817f011443412653472d2fd
Gabriel Bianchi <gdbianchii@gmail.com>
2024-08-09 14:51:08 -0300

    Fix teamInvite/send email not working

--- packages/api/src/utils/locales.ts
+++ packages/api/src/utils/locales.ts
@@ -2,11 +2,13 @@ import type { z, ZodSchema } from "zod";
 import { cookies } from "next/headers";
 import { getTranslations } from "next-intl/server";
 
+import type { locales } from "@kdx/locales";
 import { defaultLocale } from "@kdx/locales";
 import { createI18nZodErrors } from "@kdx/validators/useI18nZodErrors";
 
 export const getLocaleBasedOnCookie = () =>
-  cookies().get("NEXT_LOCALE")?.value ?? defaultLocale;
+  (cookies().get("NEXT_LOCALE")?.value ??
+    defaultLocale) as (typeof locales)[number];
 
 type SchemaGetterFromT<S extends ZodSchema> = (
   t: Awaited<ReturnType<typeof getTranslations>>,
