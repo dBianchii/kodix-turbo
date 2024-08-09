8aca822d530906e1c920c360862b9bb9cb44b773
Gabriel Bianchi <gdbianchii@gmail.com>
2024-08-09 08:43:25 -0300

    yes

--- apps/kdx/src/app/[locale]/team/settings/general/_components/delete-team-name-card-client.tsx
+++ apps/kdx/src/app/[locale]/team/settings/general/_components/delete-team-name-card-client.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { LuLoader2 } from "react-icons/lu";
+import { z } from "zod";
 
 import { useTranslations } from "@kdx/locales/next-intl/client";
 import { useRouter } from "@kdx/locales/next-intl/navigation";
 import { getErrorMessage } from "@kdx/shared";
-import { cn } from "@kdx/ui";
 import {
   AlertDialog,
-  AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
@@ -52,51 +50,68 @@ export function DeleteTeamCardClient({
   useI18nZodErrors();
   const t = useTranslations();
 
+  const deleteMyTeamString = t("delete my team");
+
   const form = useForm({
-    schema: ZDeleteTeamInputSchema,
+    schema: ZDeleteTeamInputSchema.extend({
+      teamNameConfirmation: z.literal(teamName, {
+        message: t("Team name does not match"),
+      }),
+      verification: z.literal(deleteMyTeamString),
+    }),
   });
 
   const router = useRouter();
   const mutation = api.team.deleteTeam.useMutation({
     onSuccess: () => {
+      router.push("/team");
       router.refresh();
     },
   });
 
   return (
     <Card className="w-full border-destructive text-left">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit((data) => {
-            toast.promise(mutation.mutateAsync(data), {
-              loading: `${t("Saving")}...`,
-              success: t("Team name saved successfully"),
-              error: (err) => {
-                return getErrorMessage(err);
-              },
-            });
-          })}
-        >
-          <CardHeader>
-            <CardTitle>{t("Delete team")}</CardTitle>
-            <CardDescription>
-              {t("Before deleting your team you must remove all members")}
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5"></div>
-            </div>
-          </CardContent>
-          <CardFooter className="flex justify-end border-t border-destructive bg-destructive/40 px-6 py-4">
-            {/* <CardDescription>
+      <CardHeader>
+        <CardTitle>{t("Delete team")}</CardTitle>
+        <CardDescription>
+          {t("Before deleting your team you must remove all members")}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5"></div>
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-end border-t border-destructive bg-destructive/40 px-6 py-4">
+        {/* <CardDescription>
               {t("Please use 32 characters at maximum")}
             </CardDescription> */}
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button variant={"destructive"}>{t("Delete team")}</Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
+        <AlertDialog
+          onOpenChange={(open) => {
+            form.reset();
+            return open;
+          }}
+        >
+          <AlertDialogTrigger asChild>
+            <Button variant={"destructive"}>{t("Delete team")}</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <Form {...form}>
+              <form
+                className="flex flex-col gap-4"
+                onSubmit={form.handleSubmit((values) => {
+                  toast.promise(
+                    mutation.mutateAsync({
+                      teamNameConfirmation: values.teamNameConfirmation,
+                    }),
+                    {
+                      loading: t("Deleting team..."),
+                      success: t("Team deleted successfully"),
+                      error: (err) => getErrorMessage(err),
+                    },
+                  );
+                })}
+              >
                 <AlertDialogHeader>
                   <AlertDialogTitle>
                     {t.rich("You are deleting your team teamName", {
@@ -108,17 +123,58 @@ export function DeleteTeamCardClient({
                       "All data related to your team will be permanently lost and cannot be recovered",
                     )}
                   </AlertDialogDescription>
+                  <div className="flex gap-2 rounded-md bg-destructive/40 p-3 text-sm text-red-400">
+                    <span className="font-bold">{t("Warning")}:</span>
+                    <span>
+                      {t("This action is not reversible please be certain")}
+                    </span>
+                  </div>
                 </AlertDialogHeader>
-                <div>ey</div>
-                <AlertDialogFooter className="justify-between">
+                <div className="bg-card/40 py-8">
+                  <FormField
+                    control={form.control}
+                    name="teamNameConfirmation"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-muted-foreground">
+                          {t.rich("Enter the team name teamName to continue", {
+                            teamName: () => <b>{teamName}</b>,
+                          })}
+                        </FormLabel>
+                        <FormControl>
+                          <Input {...field} disabled={!canEdit} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="verification"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-muted-foreground">
+                          {t.rich("To verify please type KEY", {
+                            deleteMyTeam: () => <b>{deleteMyTeamString}</b>,
+                          })}
+                        </FormLabel>
+                        <FormControl>
+                          <Input {...field} disabled={!canEdit} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <AlertDialogFooter className="gap-3 sm:justify-between">
                   <AlertDialogCancel>{t("Cancel")}</AlertDialogCancel>
-                  <AlertDialogAction>{t("Continue")}</AlertDialogAction>
+                  <Button type="submit">{t("Continue")}</Button>
                 </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </CardFooter>
-        </form>
-      </Form>
+              </form>
+            </Form>
+          </AlertDialogContent>
+        </AlertDialog>
+      </CardFooter>
     </Card>
   );
 }
