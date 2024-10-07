8ca3413c8675183be08e6298048eea980290d370
Gabriel Bianchi <gdbianchii@gmail.com>
2024-10-07 16:27:19 -0300

    eslint is awesome

--- packages/db/src/schema/utils.ts
+++ packages/db/src/schema/utils.ts
@@ -1,16 +1,15 @@
 //? This file is used to create common values that can be used in the schema files.
 
 import type { mysqlTable } from "drizzle-orm/mysql-core";
-import { decimal } from "drizzle-orm/mysql-core";
 
 import { nanoid, NANOID_SIZE } from "../nanoid";
 import { teams } from "./teams";
 
 export const DEFAULTLENGTH = 255;
 
-export const moneyDecimal = decimal;
-
 type THelper = Parameters<Parameters<typeof mysqlTable>["1"]>["0"];
+
+// export const moneyDecimal = (t: THelper) => t.decimal;
 
 /** Most of the time, we want to delete a record when the associated team is deleted. This centralizes the reference */
 export const teamIdReferenceCascadeDelete = (t: THelper) =>
