53ab181df2fd0cc608274eeac92c1769a6958c03
Gabriel Bianchi <gdbianchii@gmail.com>
2024-10-13 19:46:58 -0300

    fixes

--- apps/care-expo/src/app/home/(tabs)/notifications/config/index.tsx
+++ apps/care-expo/src/app/home/(tabs)/notifications/config/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Stack } from "expo-router";
 import { AlertCircle } from "@tamagui/lucide-icons";
 import { useToastController } from "@tamagui/toast";
@@ -13,15 +14,7 @@ import {
 } from "tamagui";
 
 import { getErrorMessage, kodixCareAppId } from "@kdx/shared";
-import { ZSaveUserAppTeamConfigInputSchema } from "@kdx/validators/trpc/app";
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  useForm,
-} from "~/components/form";
 import { defaultPadding } from "~/components/safe-area-view";
 import { api } from "~/utils/api";
 
@@ -85,67 +78,46 @@ function NotificationSettingsForm({
       });
     },
   });
-  const form = useForm({
-    schema: ZSaveUserAppTeamConfigInputSchema,
-    defaultValues: {
+  const [
+    sendNotificationsForDelayedTasks,
+    setSendNotificationsForDelayedTasks,
+  ] = useState(initialSendNotificationsForDelayedTasks);
+
+  const handleSave = () => {
+    setSendNotificationsForDelayedTasks((prev) => !prev);
+    mutation.mutate({
       appId: kodixCareAppId,
       config: {
-        sendNotificationsForDelayedTasks:
-          initialSendNotificationsForDelayedTasks,
+        sendNotificationsForDelayedTasks: !sendNotificationsForDelayedTasks,
       },
-    },
-  });
+    });
+  };
 
   return (
-    <Form {...form}>
-      <FormField
-        control={form.control}
-        name="config.sendNotificationsForDelayedTasks"
-        render={({ field }) => (
-          <FormItem>
-            <FormControl>
-              <YGroup alignSelf="center" bordered size="$4">
-                <YGroup.Item>
-                  <ListItem
-                    icon={<AlertCircle color={"orange"} size={"$2"} />}
-                    title="Tarefas críticas"
-                    subTitle={
-                      <SizableText
-                        size={"$2"}
-                        color={"$gray11Dark"}
-                        lineHeight={"$1"}
-                      >
-                        Receber notificações de tarefas críticas que estão
-                        atrasadas.
-                      </SizableText>
-                    }
-                    iconAfter={
-                      <View>
-                        <Switch
-                          checked={field.value}
-                          defaultChecked={
-                            initialSendNotificationsForDelayedTasks
-                          }
-                          onCheckedChange={(checked) => {
-                            mutation.mutate({
-                              appId: kodixCareAppId,
-                              config: {
-                                sendNotificationsForDelayedTasks: checked,
-                              },
-                            });
-                          }}
-                        >
-                          <Switch.Thumb animation="quicker" />
-                        </Switch>
-                      </View>
-                    }
-                  />
-                </YGroup.Item>
-              </YGroup>
-            </FormControl>
-          </FormItem>
-        )}
-      />
-    </Form>
+    <YGroup alignSelf="center" bordered size="$4">
+      <YGroup.Item>
+        <ListItem
+          icon={<AlertCircle color={"orange"} size={"$2"} />}
+          title={"Tarefas críticas"}
+          onPress={handleSave}
+          subTitle={
+            <SizableText size={"$2"} color={"$gray11Dark"} lineHeight={"$1"}>
+              Receber notificações de tarefas críticas que estão atrasadas.
+            </SizableText>
+          }
+          iconAfter={
+            <View>
+              <Switch
+                defaultChecked={initialSendNotificationsForDelayedTasks}
+                checked={sendNotificationsForDelayedTasks}
+                onCheckedChange={handleSave}
+              >
+                <Switch.Thumb animation="quicker" />
+              </Switch>
+            </View>
+          }
+        />
+      </YGroup.Item>
+    </YGroup>
   );
 }
