c5d00b35ef82637e4348829d12789d9cfb325608
Gabriel Bianchi <gdbianchii@gmail.com>
2024-10-15 16:50:29 -0300

    teste remote caching vercel

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -15,3 +15,5 @@ export const KDX_PRODUCTION_URL = "https://www.kodix.com.br";
 export const KDX_VERCEL_PROJECT_NAME = "kdx";
 export const KODIX_NOTIFICATION_FROM_EMAIL =
   "Kodix <notification@kodix.com.br>"; //? Default 'from' email for notifications
+
+export const testCache = "testeeee";
