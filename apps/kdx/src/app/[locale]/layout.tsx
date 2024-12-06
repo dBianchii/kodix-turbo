969a1fbb3fa1619d91037ecb8ccd0ea4007a512d
Gabriel Bianchi <gdbianchii@gmail.com>
2024-12-06 11:42:53 -0300

    import PostHogPageView component with SSR disabled

--- apps/kdx/src/app/[locale]/layout.tsx
+++ apps/kdx/src/app/[locale]/layout.tsx
@@ -11,6 +11,7 @@ import { TRPCReactProvider } from "~/trpc/react";
 
 import "~/app/globals.css";
 
+import dynamic from "next/dynamic";
 import { getMessages } from "next-intl/server";
 
 import { getBaseUrl } from "@kdx/shared";
@@ -19,8 +20,14 @@ import { ThemeProvider, ThemeToggle } from "@kdx/ui/theme";
 import { Toaster } from "@kdx/ui/toast";
 
 import { CCNextIntlClientProvider } from "./_components/cc-next-intl-client-provider";
-import PostHogPageView from "./_components/posthog-page-view";
 import { CSPostHogProvider } from "./_components/posthog-provider";
+
+const PostHogPageView = dynamic(
+  () => import("./_components/posthog-page-view"),
+  {
+    ssr: false,
+  },
+);
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseUrl()),
