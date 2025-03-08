99e0d0c057944940b5ebd922e0499d4e11f6dbcf
Gabriel Bianchi <gdbianchii@gmail.com>
2025-03-08 19:43:12 -0300

    Refactor package.json dependencies to use catalog references and migrate email components to new structure

--- packages/react-email/src/team-invite.tsx
+++ packages/react-email/src/team-invite.tsx
@@ -17,8 +17,8 @@ import {
 import type { IsomorficT } from "@kdx/locales";
 import { getBaseUrl } from "@kdx/shared";
 
-import type { TMock } from "../utils";
-import { tMock } from "../utils";
+import type { TMock } from "./utils";
+import { tMock } from "./utils";
 
 const baseUrl = getBaseUrl();
 
