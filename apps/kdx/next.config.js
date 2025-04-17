5488c9d242cdcde64993725eb67d5d76e815f0e0
Gabriel Bianchi <gdbianchii@gmail.com>
2025-04-17 10:02:36 -0300

    simplify and add not-found page

--- apps/kdx/next.config.js
+++ apps/kdx/next.config.js
@@ -9,9 +9,7 @@ await createJiti(fileURLToPath(import.meta.url)).import("./src/env");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  // reactStrictMode: true,
   serverExternalPackages: ["@node-rs/argon2"],
-
   experimental: {
     serverActions:
       process.env.NODE_ENV === "development"
@@ -36,32 +34,6 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  // async headers() {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
-  // logging: {
-  //   fetches: {
-  //     fullUrl: true,
-  //   },
-  // },
 };
 
 export default withNextIntl(config);
