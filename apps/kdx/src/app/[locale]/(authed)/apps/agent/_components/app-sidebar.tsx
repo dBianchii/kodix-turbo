9937f8c3fdda2e6abef64728e57d6bda1c6af72d
Mahadeva Das <wcbianchi@gmail.com>
2025-05-11 19:25:03 -0300

    Agente-1.8

--- apps/kdx/src/app/[locale]/(authed)/apps/agent/_components/app-sidebar.tsx
+++ apps/kdx/src/app/[locale]/(authed)/apps/agent/_components/app-sidebar.tsx
@@ -1,16 +1,19 @@
 import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { useTranslations } from "next-intl";
 
+import { agentAppId } from "@kdx/shared";
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
 } from "@kdx/ui/sidebar";
+
+import { IconKodixApp } from "~/app/[locale]/_components/app/kodix-icon";
 
 // Menu items.
 const items = [
@@ -43,6 +46,7 @@ const items = [
 
 export function AppSidebar() {
   const { isMobile } = useSidebar();
+  const t = useTranslations();
   return (
     <Sidebar
       collapsible={isMobile ? "offcanvas" : "none"}
@@ -50,9 +54,12 @@ export function AppSidebar() {
     >
       <SidebarContent className="h-full">
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
+              <div className="flex items-end gap-2 py-2 font-semibold">
+                <IconKodixApp appId={agentAppId} renderText={false} size={35} />
+                <span className="text-lg">{t("Agent")}</span>
+              </div>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
--- apps/kdx/src/app/[locale]/(authed)/apps/agent/page.tsx
+++ apps/kdx/src/app/[locale]/(authed)/apps/agent/page.tsx
@@ -2,12 +2,17 @@
 
 import { useTranslations } from "next-intl";
 
-import { Separator } from "@kdx/ui/separator";
 import { SidebarProvider, SidebarTrigger } from "@kdx/ui/sidebar";
-import { H1 } from "@kdx/ui/typography";
 
 import { AppSidebar } from "./_components/app-sidebar";
 import { ChatWindow } from "./_components/chat-window";
+
+{
+  /*
+  import { H1 } from "@kdx/ui/typography"; 
+  import { Separator } from "@kdx/ui/separator";
+  */
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const t = useTranslations();
@@ -24,12 +29,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <div className="mb-4 flex items-center gap-2">
             <SidebarTrigger className="md:hidden" />
             <h1 className="text-xl font-semibold">
-              Bem‑vindo ao painel do agente
+              {t("apps.agent.welcome-agent")}
             </h1>
           </div>
 
-          <H1>{t("Agent")}</H1>
+          {/* <H1>{t("Agent")}</H1>
           <Separator className="my-4" />
+          */}
 
           {/* Área do chat cresce para preencher o espaço restante */}
           <div className="relative flex-1">
