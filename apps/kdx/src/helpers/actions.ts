5393c58b753fb62bf0023bbe7cb2ada44e85cf27
Gabriel Bianchi <gdbianchii@gmail.com>
2025-05-22 14:48:39 -0300

    test again

--- apps/kdx/src/helpers/actions.ts
+++ apps/kdx/src/helpers/actions.ts
@@ -6,6 +6,7 @@ import { auth, deleteSessionTokenCookie, invalidateSession } from "@kdx/auth";
 
 import { redirect } from "~/i18n/routing";
 
+//test again
 export async function signOutAction() {
   const { session } = await auth();
   if (!session) {
