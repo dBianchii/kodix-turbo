eb6cec0ccfc82dba7967e75d93aa158e6aa15580
Gabriel Bianchi <gdbianchii@gmail.com>
2025-09-16 22:47:06 -0300

    fix

--- packages/api/src/global.d.ts
+++ packages/api/src/global.d.ts
@@ -1,11 +1,12 @@
 // Use type safe message keys with `next-intl`
 import type { formats } from "@kdx/locales";
 
-//? @kdx/api should only use "api" namespace
+//? @kdx/api needs access to both "api" and "validators" namespaces since it imports validators
 type ApiMessages = typeof import("../../locales/src/messages/api/en.json");
+type ValidatorsMessages = typeof import("../../locales/src/messages/validators/en.json");
 type Formats = typeof formats;
 
 declare global {
-  type IntlMessages = ApiMessages;
+  type IntlMessages = ApiMessages & ValidatorsMessages;
   type IntlFormats = Formats;
 }
