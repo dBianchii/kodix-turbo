b34589320a2626f883651c56f023c6053a4e759d
Gabriel Bianchi <gdbianchii@gmail.com>
2025-09-16 22:47:30 -0300

    ok

--- packages/api/src/global.d.ts
+++ packages/api/src/global.d.ts
@@ -3,7 +3,8 @@ import type { formats } from "@kdx/locales";
 
 //? @kdx/api needs access to both "api" and "validators" namespaces since it imports validators
 type ApiMessages = typeof import("../../locales/src/messages/api/en.json");
-type ValidatorsMessages = typeof import("../../locales/src/messages/validators/en.json");
+type ValidatorsMessages =
+  typeof import("../../locales/src/messages/validators/en.json");
 type Formats = typeof formats;
 
 declare global {
