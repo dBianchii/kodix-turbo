e97c4d43efadaa030da9ce5bfc83c48fd1c9f8e3
Gabriel Bianchi <gdbianchii@gmail.com>
2025-10-25 22:25:57 -0300

    enableExceptionAutocapture

--- apps/cash/app/src/lib/posthog-server.ts
+++ apps/cash/app/src/lib/posthog-server.ts
@@ -4,6 +4,7 @@ import { PostHog } from "posthog-node";
 export function getPostHogServer() {
   // biome-ignore lint/style/noNonNullAssertion: Fix me
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    enableExceptionAutocapture: true,
     flushAt: 1,
     flushInterval: 0,
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
